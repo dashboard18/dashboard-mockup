f46929bc892441210c3f05c909eb54e293c2192d
vinverdy <dev.vinverdy@gmail.com>
2018-07-14 21:02:25 +0800

    feature: upgrade angular 6

--- apps/backend/src/bootstrap/app.module.ts
+++ apps/backend/src/bootstrap/app.module.ts
@@ -1,42 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
-
-import { environment } from '../../../../environments/environment';
 import { UbudTemplateModule } from '@ubud/template';
 import { UbudSidebarMenuModule } from '@ubud/menus';
 import { UbudTooltipModule } from '@ubud/tooltip';
-import { UbudUtilitiesModule } from '@ubud/utilities';
 import { UbudDropdownModule } from '@ubud/dropdown';
 import { UbudUserControlModule } from '@ubud/user-control';
 import { routing } from './app.routes';
 import { StoreModule } from '@ngrx/store';
-import { appReducer } from '../domains/reducer';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
-        StoreModule.forRoot(appReducer),
+        StoreModule.forRoot({}),
         EffectsModule.forRoot([]),
-        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
         StoreDevtoolsModule.instrument(<any>{
             maxAgent: 25,
             logOnly: environment.production,
         }),
         StoreRouterConnectingModule,
         routing,
-        UbudUtilitiesModule,
         UbudTemplateModule,
         UbudSidebarMenuModule,
         UbudTooltipModule,
         UbudDropdownModule,
         UbudUserControlModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ],
     providers: [],
     bootstrap: [AppComponent],
